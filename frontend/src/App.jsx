63713e2e2feaa663dd415f1582e5eb828b1492af
Sohan Bhadalkar <chimpbowl502@gmail.com>
2026-01-02 16:02:54 +0530

    Describe the change here

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -11,6 +11,7 @@ async function sendToAPI(file) {
 
   const API_BASE_URL =
     import.meta.env.VITE_API_URL || "http://localhost:5000";
+  console.log("VITE_API_URL =", import.meta.env.VITE_API_URL);
 
   const res = await fetch(`${API_BASE_URL}/`, {
     method: "POST",
